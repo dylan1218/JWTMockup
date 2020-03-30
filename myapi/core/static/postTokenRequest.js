8f942bf53d87c177dc25198e55e66123c17ab2cb
dylan1218@live.com <dylan1218@live.com>
2020-03-30 13:09:25 -0400

    refresh token handling and protected request handling

--- myapi/core/static/postTokenRequest.js
+++ myapi/core/static/postTokenRequest.js
@@ -1,19 +1,53 @@
-/*document.getElementById('loginBtn').addEventListener('click', function(e) {
-    postTokenRequest()
-});
+/*
+initialSetJWTToken sets the access and refresh tokens as a cookies.
+This function is called after the response from postTokenRequest is recieved.
 */
-function doSomethingWith(strResponse) {
-    console.log(strResponse.refresh)
-    console.log(strResponse.access)
-    setCookie("refresh", strResponse.refresh, 1)
-    setCookie("access", strResponse.access, 1)
+function initialSetJWTToken(responseObject) {
+    console.log(responseObject.refresh)
+    console.log(responseObject.access)
+    setCookie("refresh", responseObject.refresh, 1)
+    setCookie("access", responseObject.access, 1)
 }
 
+
+function protectedRequest() {
+    console.log("protected request clicked")
+    $.ajax({
+        url: 'http://127.0.0.1:8000/hello/',
+        type: 'GET',
+        dataType: "json",
+        data: "", //{identifier: "A12345", password: "112233"}
+        beforeSend: function(xhr) { xhr.setRequestHeader('Authorization', 'Bearer ' + getCookie("access")); },
+        success: function(response, httpObj) {
+            if (httpObj.status = 200) {
+                alert("Sucessfully accessed endpoint with token")
+            }
+
+        },
+        error: function(httpObj, textStatus) {
+            if (httpObj.status = 401) {
+                console.log(httpObj.status)
+                alert("Bad token")
+                refreshTokenRequest();
+                //protectedRequest(); to think about a way to recursively call after refresh, but limit the amount of attempts
+            } //Note: refreshTokenRequest has error handling to go to login, which helps prevent some of the issues with too much recursion.
+        }
+    });
+}
+/* 
+postTokenRequest on called on user click
+This function sends a username and password
+in a form object to the JWT token request endpoint.
+On receipt of the response from the post the 
+initialSetJWTToken function is called. 
+*/
 function postTokenRequest() {
     console.log("clicked")
     var formData = new FormData();
-    formData.append("username", "dylan")
-    formData.append("password", "badpassword")
+    usernameVar = $('#loginForm').find('input[name="username"]').val();
+    passwordVar = $('#loginForm').find('input[name="password"]').val();
+    formData.append("username", usernameVar) // value is hardcoded, should query from the form
+    formData.append("password", passwordVar) // value is hardcoded, should query from the form
     $.ajax({
         url: 'http://127.0.0.1:8000/api/token/',
         type: 'POST',
@@ -22,11 +56,41 @@ function postTokenRequest() {
         processData: false,
         contentType: false, //beforeSend: function(xhr){xhr.setRequestHeader('X-Header', 'header-value');},    
         success: function(response) {
-            doSomethingWith(response)
+            initialSetJWTToken(response)
+        }
+    });
+};
+/*
+This function is called if there is an authentication error with the initial protected
+endpoint. This function gets the refresh token from the refresh cookies and try to obtain
+new credentials.
+
+If this is unsucessful (i.e. refresh expired) the page should redirect back 
+to the login page to obtain a new pair of tokens. 
+*/
+function refreshTokenRequest() {
+    var formData = new FormData();
+    formData.append("refresh", getCookie("refresh")) // value is hardcoded, should query from the form
+    $.ajax({
+        url: 'http://127.0.0.1:8000/api/token/refresh/',
+        type: 'POST',
+        dataType: "json",
+        data: formData, //{identifier: "A12345", password: "112233"}
+        processData: false,
+        contentType: false, //beforeSend: function(xhr){xhr.setRequestHeader('X-Header', 'header-value');},    
+        success: function(response) {
+            initialSetJWTToken(response)
+        },
+        error: function() {
+            window.location.replace("http://127.0.0.1:8000/login/")
         }
     });
 };
 
+/*
+The cookie functions below are helper functions for 
+getting, setting and erasing cookies.
+*/
 function setCookie(name, value, days) {
     var expires = "";
     if (days) {
